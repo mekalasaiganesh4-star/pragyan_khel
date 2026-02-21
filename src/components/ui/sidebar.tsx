5189132902e34c1637a0e44169795ae05f8f0eb2
Mekala sai ganesh <mekalasaiganesh4@gmail.com>
2026-02-21 06:48:10 +0000

    A tree hydrated but some attributes of the server rendered HTML didn't m

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -650,9 +650,11 @@ const SidebarMenuSkeleton = React.forwardRef<
     showIcon?: boolean
   }
 >(({ className, showIcon = false, ...props }, ref) => {
-  // Random width between 50 to 90%.
-  const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
+  // Random width between 50 to 90% generated after hydration.
+  const [width, setWidth] = React.useState<string>("70%")
+
+  React.useEffect(() => {
+    setWidth(`${Math.floor(Math.random() * 40) + 50}%`)
   }, [])
 
   return (
